46cede8c276bd3b0594417c9837a09c6b659ac14
Ismael Volkmann <ismavolk@gmail.com>
2019-05-14 10:15:54 -0300

    Add progress bar

--- pomo.sh
+++ pomo.sh
@@ -1,37 +1,83 @@
 #!/bin/bash
 
-# criado por ismavolk
+# By ismavolk
 
 thisPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P );
 pomoCounterFile="${thisPath}/.pomoCounterFile";
 soundFile="${thisPath}/sound.wav";
 
-# uso: 0timer $((25*60)) (para 25 minutos
-# uso: 0timer 20 (para 20 segundos)
-function 0timer()
+# uso: pomoTimer $((25*60)) (para 25 minutos
+# uso: pomoTimer 20 (para 20 segundos)
+function pomoTimer()
 {
     if [ "${1}" == "--help" ] || [ "${1}" == "-h" ] || [ "${1}" == "?" ] || [ "${1}" == "" ]; then
-        echo "uso: 0timer $((25*60)) && xmessage \"O pomodoro terminou\""
+        echo "uso: pomoTimer $((25*60)) && xmessage \"O pomodoro terminou\""
     else
+        date0=$((`date +%s`));
         date1=$((`date +%s` + $1));
+        totalTime=$(date -u --date @$(($1)) +%M:%S);
+        secsTot=$(date -u --date @$(($1)) +%s);
+        progressBarSize=50;
+
+        LC_NUMERIC="en_US.UTF-8";
+
         while [ "$date1" -ge `date +%s` ]; do
-            echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
-            sleep 0.1
+            reverseTime=$(date -u --date @$(($date1 - `date +%s`)) +%M:%S);
+            timeElapsed=$(date -u --date @$((`date +%s` - $date0)) +%M:%S);
+            secsElapsed=$(date -u --date @$((`date +%s` - $date0)) +%s);
+
+            #perc number %
+            perc=$(bc <<< "scale=2; ($secsElapsed/$secsTot)*100");
+            perc=`printf "%0.0f\n" $perc`;
+
+            # progress bar
+            percBar=$(bc <<< "scale=2; ($perc/100*$progressBarSize)");
+            percBar=`printf "%0.0f\n" $percBar`;
+
+            len=0;
+            strProgress=""
+            while [ $len -lt $percBar ];
+            do
+                strProgress=$strProgress"=";
+                let len=len+1;
+            done
+
+
+            strProgress=$strProgress">";
+
+            len=`echo ${#strProgress}`
+            while [ $len -lt $progressBarSize ];
+            do
+                strProgress=$strProgress" ";
+                let len=len+1;
+            done
+            # /progress bar
+
+#            pomoImgBlink="  ";
+#            if [ `echo "$secsElapsed % 2" | bc` -eq 0 ]; then
+#                pomoImgBlink="\U1F345";
+#            fi
+
+            echo -ne "$2: $timeElapsed $perc%[$strProgress] -$reverseTime / $totalTime\r";
+
+            sleep 1
         done
     fi
 }
 
 function pomodoro()
 {
+    clear;
+
     # se o arquivo contador não existe, cria ele
     if [ ! -f $pomoCounterFile ]; then
-        echo 1 > $pomoCounterFile;
+        echo 0 > $pomoCounterFile;
     fi
 
-    pomodoroCounter=`cat ${pomoCounterFile}`;
+    completedPomodoroCounter=`cat ${pomoCounterFile}`;
 
     if [ "${1}" == "-r" ]; then
-        echo 1 > $pomoCounterFile;
+        echo 0 > $pomoCounterFile;
         echo "Pomodoro counter reseted"
         return;
     fi
@@ -46,16 +92,27 @@ function pomodoro()
         return;
     fi
 
-    if [ "${1}" == "" ] || [ "${1}" == "-w" ]; then
-        echo "--- pomodoro ${pomodoroCounter} (completed: $((pomodoroCounter-1))) ---";
-#        0timer 1
-        0timer $((25*60)); # 25 minutos de pomodoro
+    echo -n "Completed: $completedPomodoroCounter";
+    if [ $completedPomodoroCounter -gt 0 ]; then
 
-        ( xmessage "--- pomodoro ${pomodoroCounter} finished ---" & ) > /dev/null 2>&1;
+        j=0
+        while [ $j -lt $completedPomodoroCounter ]; do
+            echo -ne " \U1F345 ";
+            j=$(( j + 1 ))
+        done
+    fi
+
+    echo "";
+
+    if [ "${1}" == "" ] || [ "${1}" == "-w" ]; then
+#        pomoTimer 1
+        pomoTimer $((25*60)) "Pomodoro";
+
+        ( xmessage "### pomodoro $((completedPomodoroCounter+1)) finished ###" & ) > /dev/null 2>&1;
         aplay -q "$soundFile";
 
-        pomodoroCounter=$((pomodoroCounter+1));
-        echo $pomodoroCounter > $pomoCounterFile;
+        completedPomodoroCounter=$((completedPomodoroCounter+1));
+        echo $completedPomodoroCounter > $pomoCounterFile;
 
         if [ "${1}" == "-w" ]; then
             return;
@@ -64,24 +121,20 @@ function pomodoro()
         pomodoro -b; # inicia o contador de tempo de descanso imediatamente
     else
         if [ "${1}" == "-b" ]; then
-            if [ $pomodoroCounter -lt 5 ]; then
+            if [ $completedPomodoroCounter -lt 4 ]; then
                 pomodoro -sb; # 5 minutos de descanso
             else
                 pomodoro -lb; # após 5 pomodoros, 15 minutos de descanso
             fi
         elif [ "${1}" == "-sb" ]; then
-            echo "--- short break ---";
+            pomoTimer $((5*60)) "Short break"; # 5 minutos de descanso
 
-            0timer $((5*60)); # 5 minutos de descanso
-
-            ( xmessage "--- End of the rest, to work! ---" & ) > /dev/null 2>&1;
+            ( xmessage "### End of the rest, to work! ###" & ) > /dev/null 2>&1;
             aplay -q "$soundFile";
         elif [ "${1}" == "-lb" ]; then
-            echo "--- long break ---";
+            pomoTimer $((15*60)) "Long break"; # após 5 pomodoros, 15 minutos de descanso
 
-            0timer $((15*60)); # após 5 pomodoros, 15 minutos de descanso
-
-            ( xmessage "--- End of the rest, to work! ---" & ) > /dev/null 2>&1;
+            ( xmessage "### End of the rest, to work! ###" & ) > /dev/null 2>&1;
             aplay -q "$soundFile";
         fi
 
